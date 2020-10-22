99d54e612ac5e94c2444b9d24e4d9af6689dc0fb
Owen Roth <oroth8@comcast.net>
2020-10-21 21:36:14 -0500

    added updated readme

--- Develop/app.js
+++ Develop/app.js
@@ -5,6 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -118,10 +119,6 @@ inquirer
                   throw err
               })
       }
-      
-          
-
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee
